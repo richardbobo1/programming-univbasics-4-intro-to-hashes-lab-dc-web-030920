f372f079be26cf87d72592294ff11e3bae455d94
Richard Bobo <richard.bobo@gmail.com>
2020-03-01 22:37:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -92,5 +92,3 @@ return hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
-
-update_counting_hash(new_hash, "hello")
